c27aaa3d94c47a0aefe17125770e22de0e766788
Refkinscallv <refkinscallv@gmail.com>
2026-01-15 08:14:23 +0700

    Update routes

--- app/routes/register.route.js
+++ app/routes/register.route.js
@@ -1,15 +1,6 @@
 'use strict'
 
-const Logger = require('@core/logger.core')
+require('@app/routes/web.route')
+require('@app/routes/api.route')
 
-try {
-    Logger.info('routes', 'loading routes...')
-
-    require('@app/routes/web.route')
-    require('@app/routes/api.route')
-
-    Logger.info('routes', 'routes loaded successfully')
-} catch (err) {
-    Logger.set(err, 'routes')
-    throw err
-}
+module.exports = {}
